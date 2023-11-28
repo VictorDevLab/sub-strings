b23d64d06e6b8881c0697599a4d8595cced82745
Victor Koome <koomev786@gmail.com>
2023-11-28 18:53:35 +0300

    solving the substring

--- sub.rb
+++ sub.rb
@@ -1,1 +1,11 @@
-puts "hello word"
+
+def substrings (word, dictionary)
+
+
+end
+
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+   ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+ substrings("Howdy partner, sit down! How's it going?", dictionary)
