eb6e6bdd52d60dce4110d0df6bc52cdf2a18cbd0
Victor Koome <koomev786@gmail.com>
2023-11-29 22:19:29 +0300

    refactored code for readability

--- sub.rb
+++ sub.rb
@@ -1,7 +1,20 @@
 
 def substrings (word, dictionary)
+hash = {}
+string_array = word.downcase.split
 
-
+string_array.each do |i|
+    dictionary.each do |w|
+        if i.include?(w)
+            if hash.has_key?(w)
+                hash[w] += 1
+            else
+                hash[w] = 1
+            end
+        end
+    end
+end
+puts hash
 end
 
 
