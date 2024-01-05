b7b63824166564f81f28f3d5ab0f0f3a0b3c3262
Victor Koome <koomev786@gmail.com>
2024-01-05 21:44:16 +0300

    implement a substring method

--- sub.rb
+++ sub.rb
@@ -19,6 +19,8 @@ end
 
 
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-   ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
- substrings("Howdy partner, sit down! How's it going?", dictionary)
+dictionary = ["below","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+
+
+ substrings("below", dictionary)
